d8f2c689fbf650365cef963a4393c0c2eaae9151
emre nogay <64332775+emrenogay@users.noreply.github.com>
2023-12-03 21:41:32 +0300

    Script

---
+++ script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Dosya yolu
+domain_file="/home/domains.txt"
+
+
+declare -a domains=()
+
+while IFS= read -r domain || [ -n "$domain" ]; do
+  domains+=("$domain")
+done < "$domain_file"
+
+
+for domain in "${domains[@]}"; do
+  
+  username=$(/scripts/whoowns "$domain")
+
+  if [ -n "$username" ]; then
+    echo "Starting backup for $username"
+    /scripts/pkgacct "$username"
+    echo "Done for $username"
+  else
+    echo "Username not found for $domain"
+  fi
+done
